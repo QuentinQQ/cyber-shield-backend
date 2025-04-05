ccf6ffd52a49d5e2b00670c1277b5b6a04f4d927
QuentinQ <quentin.zt.qian@gmail.com>
2025-04-06 01:04:55 +1100

    test github actions

--- src/feed-game/dto/update-feed-game.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateFeedGameDto } from './create-feed-game.dto';
-
-export class UpdateFeedGameDto extends PartialType(CreateFeedGameDto) {}
